5aacaff2fd6ca458a06c24bc19fdbb3957ec4f6a
annacb21 <annacb21@gmail.com>
2020-10-31 12:44:00 +0100

    creato html e php navbar

---
+++ resources/templates/front/nav.php
@@ -0,0 +1,11 @@
+<div id="Navbar">
+  <a href="#" id="Logo"><img src="../public/images/logo.png" alt=""></a>
+  <div>
+    <a class="active" href="#">Home</a>
+    <a href="#">Aree di attività</a>
+    <a href="#">Team</a>
+    <a href="#">Sedi</a>
+    <a href="#">Pubblicazioni ed eventi</a>
+    <a href="#">Contatti</a>
+  </div>
+</div>
