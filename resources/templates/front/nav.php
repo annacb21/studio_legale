b035625f4d93ec1214df9c14a2a95030f2dd8d02
annacb21 <annacb21@gmail.com>
2020-11-03 12:13:19 +0100

    sistemata navbar

--- resources/templates/front/nav.php
+++ resources/templates/front/nav.php
@@ -1,11 +1,19 @@
-<div id="Navbar">
-  <a href="#" id="Logo"><img src="../public/images/logo.png" alt=""></a>
-  <div>
+<div id="navbar">
+
+  <a href="#" id="logo"><img src="../public/images/logo.png" alt="logo studio legale Turlon"></a>
+  <div id="navbar-items">
     <a class="active" href="#">Home</a>
+    <div id="dropdown">
+      <button>Sedi <i class="fa fa-caret-down"></i></button>
+      <div id="dropdown-content">
+        <a href="#">Padova</a>
+        <a href="#">Roma</a>
+      </div>
+    </div>
     <a href="#">Aree di attività</a>
     <a href="#">Team</a>
-    <a href="#">Sedi</a>
     <a href="#">Pubblicazioni ed eventi</a>
     <a href="#">Contatti</a>
   </div>
+
 </div>
