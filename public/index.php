5759dde64f3a9481f367830aa3b737b42019fcb1
annacb21 <annacb21@gmail.com>
2021-03-05 11:20:31 +0100

    reset sito

--- public/index.php
+++
@@ -1,9 +0,0 @@
-<?php require_once("../resources/config.php"); ?>
-
-<!-- HEADER AND NAVBAR -->
-<?php include(TEMPLATE_FRONT . DS . "intro.php"); ?>
-
-    <?php show_main_content(); ?>
-
-<!-- FOOTER -->
-<?php include(TEMPLATE_FRONT . DS . "footer.php"); ?>
