5d044d7dfa5da17626b8e6ea04a6277d1904598c
annacb21 <annacb21@gmail.com>
2021-03-15 16:38:51 +0100

    aggiunti controlli Aria

--- js/active.js
+++ js/active.js
@@ -4,7 +4,9 @@ var links = linksContainer.getElementsByClassName("nav-link");
 for (var i = 0; i < links.length; i++) {
     links[i].addEventListener("click", function() {
     var current = document.getElementsByClassName("active");
+    current[0].removeAttribute("aria-current");
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
+    this.setAttribute("aria-current", "page");
   });
 }
