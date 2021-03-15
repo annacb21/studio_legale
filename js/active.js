86d0d807ff82e2ef0bed97ce87b86c824e48598f
annacb21 <annacb21@gmail.com>
2021-03-15 13:05:00 +0100

    creato file js per link attivi navbar

---
+++ js/active.js
@@ -0,0 +1,10 @@
+var linksContainer = document.getElementById("navbarLinks");
+var links = linksContainer.getElementsByClassName("nav-link");
+
+for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
