86738a2b8141fec284a8f807e8372a088a80d7ad
annacb21 <annacb21@gmail.com>
2021-03-16 16:52:08 +0100

    caricati files phpMailer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '949d2ffb90112e3fe9ce7ffd100ef269ac8d4ce9',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '949d2ffb90112e3fe9ce7ffd100ef269ac8d4ce9',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.2.0',
+      'version' => '6.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e38888a75c070304ca5514197d4847a59a5c853f',
+    ),
+  ),
+);
