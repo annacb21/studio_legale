fb95bfacce9ca0997d79fef1a094b7c30347115e
annacb21 <annacb21@gmail.com>
2021-03-16 16:50:23 +0100

    creato footer php dinamico

---
+++ resources/templates/front/footer.php
@@ -0,0 +1,49 @@
+<footer class="bg-dark text-white">
+    <div class="row">
+        <div class="col-lg-4">
+            <div>
+                <img src="images/logo_light.svg" alt="logo bianco">
+                <h4 class="d-inline">Studio Legale Turlon</h4>
+            </div>
+            <ul class="nav flex-column">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Lo Studio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Le Aree di Attività</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">I Professionisti</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pubblicazioni ed eventi</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Consulenza Online</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contatti</a>
+                </li>
+            </ul>
+        </div>
+        <div class="col-lg-4">
+            <h4>Le nostre sedi</h4>
+            <div>
+                <a href="#"><h5>Sede di Padova</h5></a>
+                <p>via Emanuele Filiberto 43</p>
+                <p>+39 049 654313</p>
+            </div>
+            <div>
+                <a href="#"><h5>Sede di Roma</h5></a>
+                <p>via Alessandria 88</p>
+                <p>+39 0644 4254637</p>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <h5>Contatti</h5>
+            <p>federica.turlon@studiolegaleturlon.it</p>
+            <p>federica.turlon@servicepec.it</p>
+        </div>
+    </div>
+    <p>P.IVA 09304481006 <span>Privacy Policy</span> <span>Cookie Policy</span></p>
+</footer>
