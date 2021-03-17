10f5043f434559dde30ad64020074372bb739a65
annacb21 <annacb21@gmail.com>
2021-03-17 15:51:36 +0100

    cambiato link home

--- resources/templates/front/navbar.php
+++ resources/templates/front/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="index.html">
+        <a class="navbar-brand" href="index.php">
             <img src="images/logo.svg" alt="logo studio legale">
         </a>
         <a class="visually-hidden-focusable" href="#studio">Vai al contenuto principale</a>
@@ -10,7 +10,7 @@
         <div class="collapse navbar-collapse" id="navbarLinks">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="studioDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Lo Studio</a>
