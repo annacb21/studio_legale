1628d839f60822b6e4a690bff3fca0f8db79237f
annacb21 <annacb21@gmail.com>
2021-03-17 15:19:24 +0100

    aggiunto obj avv. turlon

--- resources/classes/prof.php
+++ resources/classes/prof.php
@@ -1,7 +1,11 @@
 <?php
 
+require_once("classes/area.php");
+
+// class def
 class Professionista {
 
+    private $id;
     private $nome;
     private $ruolo;
     private $foto;
@@ -10,13 +14,19 @@ class Professionista {
     private $aree;
 
     // constructor
-    public function __construct($nome, $ruolo, $foto, $desc, $cv, $aree) {
+    public function __construct($id, $nome, $ruolo, $foto, $desc, $cv, $aree) {
+        $this->id = $id;
         $this->nome = $nome;
         $this->ruolo = $ruolo;
         $this->foto = $foto;
         $this->desc = $desc;
         $this->cv = $cv;
         $this->aree = $aree;
+    }
+
+    // get the id
+    public function get_prof_id() {
+        return $this->id;
     }
 
     // get the name
@@ -50,5 +60,10 @@ class Professionista {
     }
 
 }
+
+// class obj
+$tDesc = "L’Avv. Federica Turlon si è laureata con lode in Giurisprudenza presso l’Università degli Studi di Padova. Ha conseguito il titolo di dottore di ricerca in ‘’Studi Giuridici Comparati ed Europei” presso l’Università degli Studi di Trento. Attualmente è professore a contratto di ‘diritto penale’ e di ‘diritto pubblico del welfare’ presso l’Università degli Studi di Padova. E’ autrice di diverse pubblicazioni in ambito giuridico penale, minorile e familiare.";
+$tAree = array($a1, $a2, $a3);
+$turlon = new Professionista("1", "Avv. Federica Turlon", "Avvocato Fondatore dello Studio", "turlon.jpg", $tDesc, "cv", $tAree);
 
 ?>
