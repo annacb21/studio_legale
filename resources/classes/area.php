15e58e67192ca231238e8d060b1e2de950df2137
annacb21 <annacb21@gmail.com>
2021-03-17 15:12:09 +0100

    aggiunto campo id

--- resources/classes/area.php
+++ resources/classes/area.php
@@ -27,15 +27,22 @@ class Caso {
 
 class Area {
 
+    private $id;
     private $nome;
     private $desc;
     private $aree;
 
     // constructor
-    public function __construct($nome, $desc, $aree) {
+    public function __construct($id, $nome, $desc, $aree) {
+        $this->id = $id;
         $this->nome = $nome;
         $this->desc = $desc;
         $this->aree = $aree;
+    }
+
+    // get the area id
+    public function get_area_id() {
+        return $this->id;
     }
 
     // get the area name
@@ -54,34 +61,34 @@ class Area {
 // $c... indica la lista delle aree tematiche specifiche di ciascuna area di attività
 // $a... indica l'area di attività
 $c1 = array(new Caso("Lorem ipsum dolor sit amet, consectetur adipiscing elit", "È quello scritto, datato e sottoscritto per intero e di pugno dal testatore."), new Caso("Lorem ipsum dolor sit amet, consectetur adipiscing elit", "È quello scritto, datato e sottoscritto per intero e di pugno dal testatore."), new Caso("Lorem ipsum dolor sit amet, consectetur adipiscing elit", "È quello scritto, datato e sottoscritto per intero e di pugno dal testatore."));
-$a1 = new Area("Diritti della persona, tutela, amministrazione di sostegno e trust", "ccc", $c1);
+$a1 = new Area("1", "Diritti della persona, tutela, amministrazione di sostegno e trust", "ccc", $c1);
 
 $c2 = array(new Caso("Lorem ipsum dolor sit amet, consectetur adipiscing elit", "È quello scritto, datato e sottoscritto per intero e di pugno dal testatore."), new Caso("Lorem ipsum dolor sit amet, consectetur adipiscing elit", "È quello scritto, datato e sottoscritto per intero e di pugno dal testatore."), new Caso("Lorem ipsum dolor sit amet, consectetur adipiscing elit", "È quello scritto, datato e sottoscritto per intero e di pugno dal testatore."));
-$a2 = new Area("Affidamento figli e protezione dei minori a rischio", "ccc", $c2);
+$a2 = new Area("2","Affidamento figli e protezione dei minori a rischio", "ccc", $c2);
 
 $c3 = array(new Caso("Lorem ipsum dolor sit amet, consectetur adipiscing elit", "È quello scritto, datato e sottoscritto per intero e di pugno dal testatore."), new Caso("Lorem ipsum dolor sit amet, consectetur adipiscing elit", "È quello scritto, datato e sottoscritto per intero e di pugno dal testatore."), new Caso("Lorem ipsum dolor sit amet, consectetur adipiscing elit", "È quello scritto, datato e sottoscritto per intero e di pugno dal testatore."));
-$a3 = new Area("Separazioni, divorzi, scioglimento unioni tra persone dello stesso sesso e convivenze", "ccc", $c3);
+$a3 = new Area("3","Separazioni, divorzi, scioglimento unioni tra persone dello stesso sesso e convivenze", "ccc", $c3);
 
 $c4 = array(new Caso("Lorem ipsum dolor sit amet, consectetur adipiscing elit", "È quello scritto, datato e sottoscritto per intero e di pugno dal testatore."), new Caso("Lorem ipsum dolor sit amet, consectetur adipiscing elit", "È quello scritto, datato e sottoscritto per intero e di pugno dal testatore."), new Caso("Lorem ipsum dolor sit amet, consectetur adipiscing elit", "È quello scritto, datato e sottoscritto per intero e di pugno dal testatore."));
-$a4 = new Area("Successioni ed eredità", "ccc", $c4);
+$a4 = new Area("4","Successioni ed eredità", "ccc", $c4);
 
 $c5 = array(new Caso("Lorem ipsum dolor sit amet, consectetur adipiscing elit", "È quello scritto, datato e sottoscritto per intero e di pugno dal testatore."), new Caso("Lorem ipsum dolor sit amet, consectetur adipiscing elit", "È quello scritto, datato e sottoscritto per intero e di pugno dal testatore."), new Caso("Lorem ipsum dolor sit amet, consectetur adipiscing elit", "È quello scritto, datato e sottoscritto per intero e di pugno dal testatore."));
-$a5 = new Area("Adozioni e procreazione medicalmente assistita", "ccc", $c5);
+$a5 = new Area("5","Adozioni e procreazione medicalmente assistita", "ccc", $c5);
 
 $c6 = array(new Caso("Lorem ipsum dolor sit amet, consectetur adipiscing elit", "È quello scritto, datato e sottoscritto per intero e di pugno dal testatore."), new Caso("Lorem ipsum dolor sit amet, consectetur adipiscing elit", "È quello scritto, datato e sottoscritto per intero e di pugno dal testatore."), new Caso("Lorem ipsum dolor sit amet, consectetur adipiscing elit", "È quello scritto, datato e sottoscritto per intero e di pugno dal testatore."));
-$a6 = new Area("Incidenti e omicidi stradali", "ccc", $c6);
+$a6 = new Area("6","Incidenti e omicidi stradali", "ccc", $c6);
 
 $c7 = array(new Caso("Lorem ipsum dolor sit amet, consectetur adipiscing elit", "È quello scritto, datato e sottoscritto per intero e di pugno dal testatore."), new Caso("Lorem ipsum dolor sit amet, consectetur adipiscing elit", "È quello scritto, datato e sottoscritto per intero e di pugno dal testatore."), new Caso("Lorem ipsum dolor sit amet, consectetur adipiscing elit", "È quello scritto, datato e sottoscritto per intero e di pugno dal testatore."));
-$a7 = new Area("Diritto penale della persona, dei minori e della famiglia", "ccc", $c7);
+$a7 = new Area("7","Diritto penale della persona, dei minori e della famiglia", "ccc", $c7);
 
 $c8 = array(new Caso("Lorem ipsum dolor sit amet, consectetur adipiscing elit", "È quello scritto, datato e sottoscritto per intero e di pugno dal testatore."), new Caso("Lorem ipsum dolor sit amet, consectetur adipiscing elit", "È quello scritto, datato e sottoscritto per intero e di pugno dal testatore."), new Caso("Lorem ipsum dolor sit amet, consectetur adipiscing elit", "È quello scritto, datato e sottoscritto per intero e di pugno dal testatore."));
-$a8 = new Area("Violenza alle donne, stalking, strumenti di tutela e ordini di protezione a favore del soggetto vulnerabile", "ccc", $c8);
+$a8 = new Area("8","Violenza alle donne, stalking, strumenti di tutela e ordini di protezione a favore del soggetto vulnerabile", "ccc", $c8);
 
 $c9 = array(new Caso("Lorem ipsum dolor sit amet, consectetur adipiscing elit", "È quello scritto, datato e sottoscritto per intero e di pugno dal testatore."), new Caso("Lorem ipsum dolor sit amet, consectetur adipiscing elit", "È quello scritto, datato e sottoscritto per intero e di pugno dal testatore."), new Caso("Lorem ipsum dolor sit amet, consectetur adipiscing elit", "È quello scritto, datato e sottoscritto per intero e di pugno dal testatore."));
-$a9 = new Area("Locazioni, recupero crediti, vendita di beni dei debitori e piani di rientro", "ccc", $c9);
+$a9 = new Area("9","Locazioni, recupero crediti, vendita di beni dei debitori e piani di rientro", "ccc", $c9);
 
 $c10 = array(new Caso("Lorem ipsum dolor sit amet, consectetur adipiscing elit", "È quello scritto, datato e sottoscritto per intero e di pugno dal testatore."), new Caso("Lorem ipsum dolor sit amet, consectetur adipiscing elit", "È quello scritto, datato e sottoscritto per intero e di pugno dal testatore."), new Caso("Lorem ipsum dolor sit amet, consectetur adipiscing elit", "È quello scritto, datato e sottoscritto per intero e di pugno dal testatore."));
-$a10 = new Area("Diritto della navigazione", "ccc", $c10);
+$a10 = new Area("10","Diritto della navigazione", "ccc", $c10);
 
 // areas list
 $aree = array($a1, $a2, $a3, $a4, $a5, $a6, $a7, $a8, $a9, $a10);
