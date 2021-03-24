b96d8c2cf7d46add682116ab6645c76f9f0377bb
annacb21 <annacb21@gmail.com>
2021-03-24 16:03:42 +0100

    creata classe database

--- resources/config.php
+++ resources/config.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once("classes/database.php");
+
 ob_start();
 
 session_start();
@@ -21,10 +23,6 @@ defined("DB_USER") ? null : define("DB_USER", "root");
 defined("DB_PASS") ? null : define("DB_PASS", "");
 
 defined("DB_NAME") ? null : define("DB_NAME", "studio_legale_db");
-
-
-$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-
 
 require_once("functions.php");
 
