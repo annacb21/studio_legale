ea6c4dba252044382228d3cc856b8e78f1af1347
annacb21 <annacb21@gmail.com>
2021-03-25 16:43:56 +0100

    modificata classe post

--- resources/classes/post.php
+++ resources/classes/post.php
@@ -12,7 +12,13 @@ class Post {
     private $testo;
 
     // constructor
-    public function __construct() {}
+    public function __construct($id, $titolo, $data, $cat, $testo) {
+        $this->id = $id;
+        $this->titolo = $titolo;
+        $this->data = $data;
+        $this->cat = $cat;
+        $this->testo = $testo;
+    }
 
     // get the id
     public function get_id() {
@@ -39,17 +45,15 @@ class Post {
         return $this->testo;
     }
 
-    // getter 
-    public function get_posts() {
-        $posts = null;
-        $query = query("SELECT * FROM post");
-        confirm($query);
-        while($row = fetch_array($query)) {
-            $posts[] = $row;
-        }
-        return $posts;
-    }
+}
 
+$posts = array();
+$query = query("SELECT * FROM post");
+confirm($query);
+$i = 0;
+while($row = fetch_array($query)) {
+    $posts[$i] = new Post($row['id'], $row['titolo'], $row['post_data'], $row['cat_id'], $row['testo']);
+    $i++;
 }
 
 
