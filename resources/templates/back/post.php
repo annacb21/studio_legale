09231858d62e0a013dcc4420a3e5b630bfa80948
annacb21 <annacb21@gmail.com>
2021-04-15 17:51:08 +0200

    aggiunta tabella post

--- resources/templates/back/post.php
+++ resources/templates/back/post.php
@@ -1,30 +1,73 @@
-<main>
-    <?php display_message(); ?>
-    <button type="button" class="btn btn-lg action-button" onclick="toggle_post_form();"><i class="fas fa-plus"></i>Aggiungi nuovo post</button>
-    <form action="" method="post" class="needs-validation mt-4" id="add-post-form" novalidate>
-        <?php addPost(); ?>
-        <div class="mb-3">
-            <label for="titolo" class="form-label">Titolo</label>
-            <input type="text" class="form-control" placeholder="Titolo post" name="titolo" id="titolo" required>
-            <div class="invalid-feedback">Inserire un titolo per il post</div>
+<?php
+// get all posts
+$post_query = query("SELECT * FROM post");
+confirm($post_query);
+$all_post = array();
+$i = 0;
+while($row = fetch_array($post_query)) {
+    $pd = $row['post_data'];
+    setlocale(LC_TIME, 'it_IT');
+    $pdate = strftime("%d %B %Y", strtotime($pd));
+    $all_post[$i] = new Post($row['id'], $row['titolo'], $pdate, $row['cat_id'], $row['testo']);
+    $i++;
+}
+?>
+
+<main class="post-main">
+    <div>
+        <?php display_message(); ?>
+        <button type="button" class="btn btn-lg action-button" onclick="toggle_post_form();"><i class="fas fa-plus"></i>Aggiungi nuovo post</button>
+        <form action="" method="post" class="needs-validation mt-4" id="add-post-form" novalidate>
+            <?php addPost(); ?>
+            <div class="mb-3">
+                <label for="titolo" class="form-label">Titolo</label>
+                <input type="text" class="form-control" placeholder="Titolo post" name="titolo" id="titolo" required>
+                <div class="invalid-feedback">Inserire un titolo per il post</div>
+            </div>
+            <div class="mb-3">
+                <label for="categoria" class="form-label">Categoria</label>
+                <select class="form-select" name="categoria" required>
+                    <option selected>Seleziona la categoria del post</option>
+                    <option value="1">News</option>
+                    <option value="2">Eventi</option>
+                    <option value="3">Pubblicazioni</option>
+                </select>
+                <div class="invalid-feedback">Selezionare una categoria per il post</div>
+            </div>
+            <div class="mb-3">
+                <label for="testo" class="form-label">Testo</label>
+                <textarea class="form-control" name="testo" id="testo" rows="5" required></textarea>
+                <div class="invalid-feedback">Inserire un testo per il post</div>
+            </div>
+            <button type="submit" name="addPost" class="btn btn-primary">Pubblica</button>
+        </form>
+    </div>
+    <div class="card mt-5">
+        <div class="card-header">
+            <h3 class="mb-0">Post pubblicati</h3>
         </div>
-        <div class="mb-3">
-            <label for="categoria" class="form-label">Categoria</label>
-            <select class="form-select" name="categoria" required>
-                <option selected>Seleziona la categoria del post</option>
-                <option value="1">News</option>
-                <option value="2">Eventi</option>
-                <option value="3">Pubblicazioni</option>
-            </select>
-            <div class="invalid-feedback">Selezionare una categoria per il post</div>
-        </div>
-        <div class="mb-3">
-            <label for="testo" class="form-label">Testo</label>
-            <textarea class="form-control" name="testo" id="testo" rows="5" required></textarea>
-            <div class="invalid-feedback">Inserire un testo per il post</div>
-        </div>
-        <button type="submit" name="addPost" class="btn btn-primary">Pubblica</button>
-    </form>
+        <div class="card-body">
+            <table>
+                <thead>
+                    <tr>
+                        <td>Titolo post</td>
+                        <td>Categoria post</td>
+                        <td>Data post</td>
+                        <td>Azioni</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach($all_post as $p): ?>
+                    <tr>
+                        <td><?php echo $p->get_title_ant(); ?></td>
+                        <td><?php echo $p->get_cat_name(); ?></td>
+                        <td><?php echo $p->get_data(); ?></td>
+                        <td class="d-flex justify-content-between"><a class="btn btn-primary" href="#" role="button">Modifica</a><a class="btn btn-danger" href="#" role="button">Elimina</a></td>
+                    </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+    </div>
 </main>
 
 <script src="https://cdn.tiny.cloud/1/vucbpm5krf4dg1gnij1tc4opj2wlgcqa8f8l0grw3xr4zkga/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
