fdc60c3d107bb85204a03bb4316bcfc95da9a038
annacb21 <annacb21@gmail.com>
2021-04-27 10:48:51 +0200

    aggiunto auto resize textarea

--- js/textEditor.js
+++ js/textEditor.js
@@ -3,7 +3,8 @@ tinymce.init({
     plugins: [
         "advlist autolink lists link image charmap print preview anchor paste",
         "searchreplace visualblocks code fullscreen",
-        "insertdatetime media table contextmenu paste"
+        "insertdatetime media table contextmenu paste",
+        "autoresize"
     ],
     toolbar: "insertfile undo redo paste | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
     entities: "160,nbsp",
